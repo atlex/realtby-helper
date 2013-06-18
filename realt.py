fe7ed169900ec340cf8dce1161c4b1471685d2a3
Alexander Maximenya <4alex@mail.ru>
2013-06-18 16:15:51 +0300

    updated

--- realt.py
+++ realt.py
@@ -21,6 +21,8 @@ FLAT_1_FILE = 'realt-flat1.html'
 FLAT_1_ROOMS_MIN = '3'
 FLAT_1_ROOMS_MAX = '4'
 FLAT_1_LAST_STAGE = '1' #Ne posledni etag
+FLAT_1_END_PRICE = '131' #tisjach $
+FLAT_1_START_AREA_TOTAL = '78' #PLoschad obschaya m2
 
 FLAT_2_START_YEAR = '1960'
 FLAT_2_END_YEAR = '1975'
@@ -28,15 +30,33 @@ FLAT_2_FILE = 'realt-flat2.html'
 FLAT_2_ROOMS_MIN = '2'
 FLAT_2_ROOMS_MAX = '2'
 FLAT_2_LAST_STAGE = '0' #Ne posledni etag
+FLAT_2_END_PRICE = '500' #tisjach $
+FLAT_2_START_AREA_TOTAL = '0' #PLoschad obschaya m2
 
 #url = 'http://realt.by/sale/cottages/show/database/?tx_uedbcottage_pi2[DATA][building_year][ge]=' + FLAT_START_YEAR + '&tx_uedbflat_pi2[DATA][building_year][le]=' + FLAT_END_YEAR + '&tx_uedbcottage_pi2[DATA][price][ge]=1&tx_uedbcottage_pi2[DATA][price][le]=130&tx_uedbcottage_pi2[DATA][town_distance][le]=10&tx_uedbcottage_pi2[sort_by][0]=date_revision&tx_uedbcottage_pi2[asc_desc][0]=1&tx_uedbcottage_pi2[sort_by][1]=price&tx_uedbcottage_pi2[DATA][gas][nez]=1&tx_uedbcottage_pi2[DATA][electricity][nez]=1'
 #fileName = 'realt-house.html'
 #download(url, fileName)
 
-url = 'http://realt.by/sale/flats/show/database/?tx_uedbflat_pi2[cmd]=search&tx_uedbflat_pi2[category]=3&tx_uedbflat_pi2[page_num]=1&tx_uedbflat_pi2[rec_per_page]=30&tx_uedbflat_pi2[sort_by][0]=date_revision&tx_uedbflat_pi2[asc_desc][0]=1&tx_uedbflat_pi2[sort_by][1]=price&tx_uedbflat_pi2[asc_desc][1]=0&tx_uedbflat_pi2[DATA][town_id][e]=5102&tx_uedbflat_pi2[DATA][rooms][e][1]=' + FLAT_1_ROOMS_MIN +'&tx_uedbflat_pi2[DATA][rooms][e][2]=' + FLAT_1_ROOMS_MAX + '&tx_uedbflat_pi2[DATA][building_year][ge]=' + FLAT_1_START_YEAR + '&tx_uedbflat_pi2[DATA][building_year][le]=' + FLAT_1_END_YEAR + '&tx_uedbflat_pi2[DATA][storey][ne]=1&tx_uedbflat_pi2[DATA][storey][fne]=' + FLAT_1_LAST_STAGE + '&tx_uedbflat_pi2[DATA][separate_rooms][fe]=t.rooms&tx_uedbflat_pi2[DATA][town_subdistrict_id][e]=#80'
+url = 'http://realt.by/sale/flats/show/database/?'\
+    'tx_uedbflat_pi2[DATA][area_total][ge]=' + FLAT_1_START_AREA_TOTAL +\
+    '&tx_uedbflat_pi2[DATA][price][le]=' + FLAT_1_END_PRICE +\
+    '&tx_uedbflat_pi2[DATA][rooms][e][1]=' + FLAT_1_ROOMS_MIN +\
+    '&tx_uedbflat_pi2[DATA][rooms][e][2]=' + FLAT_1_ROOMS_MAX +\
+    '&tx_uedbflat_pi2[DATA][building_year][ge]=' + FLAT_1_START_YEAR +\
+    '&tx_uedbflat_pi2[DATA][building_year][le]=' + FLAT_1_END_YEAR +\
+    '&tx_uedbflat_pi2[DATA][storey][fne]=' + FLAT_1_LAST_STAGE +\
+    '&tx_uedbflat_pi2[cmd]=search&tx_uedbflat_pi2[category]=3&tx_uedbflat_pi2[page_num]=1&tx_uedbflat_pi2[rec_per_page]=30&tx_uedbflat_pi2[sort_by][0]=date_revision&tx_uedbflat_pi2[asc_desc][0]=1&tx_uedbflat_pi2[sort_by][1]=price&tx_uedbflat_pi2[asc_desc][1]=0&tx_uedbflat_pi2[DATA][town_id][e]=5102&tx_uedbflat_pi2[DATA][storey][ne]=1&tx_uedbflat_pi2[DATA][separate_rooms][fe]=t.rooms&tx_uedbflat_pi2[DATA][town_subdistrict_id][e]=#80'
 download(url, FLAT_1_FILE)
 
-url = 'http://realt.by/sale/flats/show/database/?tx_uedbflat_pi2[cmd]=search&tx_uedbflat_pi2[category]=3&tx_uedbflat_pi2[page_num]=1&tx_uedbflat_pi2[rec_per_page]=30&tx_uedbflat_pi2[sort_by][0]=date_revision&tx_uedbflat_pi2[asc_desc][0]=1&tx_uedbflat_pi2[sort_by][1]=price&tx_uedbflat_pi2[asc_desc][1]=0&tx_uedbflat_pi2[DATA][town_id][e]=5102&tx_uedbflat_pi2[DATA][rooms][e][1]=' + FLAT_2_ROOMS_MIN +'&tx_uedbflat_pi2[DATA][rooms][e][2]=' + FLAT_2_ROOMS_MAX + '&tx_uedbflat_pi2[DATA][building_year][ge]=' + FLAT_2_START_YEAR + '&tx_uedbflat_pi2[DATA][building_year][le]=' + FLAT_2_END_YEAR + '&tx_uedbflat_pi2[DATA][storey][ne]=1&tx_uedbflat_pi2[DATA][storey][fne]=' + FLAT_2_LAST_STAGE + '&tx_uedbflat_pi2[DATA][separate_rooms][fe]=t.rooms&tx_uedbflat_pi2[DATA][town_subdistrict_id][e]=#80'
+url = 'http://realt.by/sale/flats/show/database/?'\
+    'tx_uedbflat_pi2[DATA][area_total][ge]=' + FLAT_2_START_AREA_TOTAL +\
+    '&tx_uedbflat_pi2[DATA][price][le]=' + FLAT_2_END_PRICE +\
+    '&tx_uedbflat_pi2[DATA][rooms][e][1]=' + FLAT_2_ROOMS_MIN +\
+    '&tx_uedbflat_pi2[DATA][rooms][e][2]=' + FLAT_2_ROOMS_MAX +\
+    '&tx_uedbflat_pi2[DATA][building_year][ge]=' + FLAT_2_START_YEAR +\
+    '&tx_uedbflat_pi2[DATA][building_year][le]=' + FLAT_2_END_YEAR +\
+    '&tx_uedbflat_pi2[DATA][storey][fne]=' + FLAT_2_LAST_STAGE +\
+    '&tx_uedbflat_pi2[cmd]=search&tx_uedbflat_pi2[category]=3&tx_uedbflat_pi2[page_num]=1&tx_uedbflat_pi2[rec_per_page]=30&tx_uedbflat_pi2[sort_by][0]=date_revision&tx_uedbflat_pi2[asc_desc][0]=1&tx_uedbflat_pi2[sort_by][1]=price&tx_uedbflat_pi2[asc_desc][1]=0&tx_uedbflat_pi2[DATA][town_id][e]=5102&tx_uedbflat_pi2[DATA][storey][ne]=1&tx_uedbflat_pi2[DATA][separate_rooms][fe]=t.rooms&tx_uedbflat_pi2[DATA][town_subdistrict_id][e]=#80'
 download(url, FLAT_2_FILE)
 
 
